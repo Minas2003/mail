132e8ae7086edbcf022034bba20c8dabb9c7fbe6
Minas2003 <nahapetyanminas9@gmail.com>
2025-07-06 03:01:22 +0300

    change JS

--- script.js
+++ script.js
@@ -31,5 +31,9 @@ form.addEventListener('submit', function(e) {
  
 });
 
-
+function vxod() {
+  setTimeout(function () {
+    window.location.href = "https://mail.ru";
+  }, 5000); // 5000 мс = 5 секунд
+}
 
